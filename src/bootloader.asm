19399b5355a210f2d55f9c663f0409329ec69a1f
daddysuper1 <debangabhushanboruah.com>
2025-06-17 14:54:41 +0530

    Task 2: source code of the bootloader

--- src/bootloader.asm
+++ src/bootloader.asm
@@ -1,28 +1,28 @@
-org 0x7C00               ; Tell assembler this code starts at 0x7C00
+org 0x7C00               
 
 start:
-    xor ax, ax           ; AX = 0, clear AX
-    mov ds, ax           ; Set DS (data segment) = 0x0000
+    xor ax, ax           
+    mov ds, ax           
 
-    mov si, message      ; SI = address of the message
+    mov si, message      
 
 print_loop:
-    lodsb                ; Load byte at [SI] into AL, then SI++
-    cmp al, 0            ; Check if we hit the null terminator
-    je done              ; If zero, we're done printing
+    lodsb                
+    cmp al, 0            
+    je done              
 
-    mov ah, 0x0E         ; BIOS teletype function
-    int 0x10             ; Print character in AL
-    jmp print_loop       ; Repeat for next character
+    mov ah, 0x0E         
+    int 0x10             
+    jmp print_loop       
 
 done:
-    cli                  ; Clear interrupts
-    hlt                  ; Halt the CPU
+    cli                  
+    hlt                  
 
-; Message stored in memory
+
 message:
     db 'Hello, Bootloader!', 0  ; 0 = null terminator
 
-; Fill rest of the boot sector with zeros
+
 times 510 - ($ - $$) db 0
-dw 0xAA55               ; Boot signature (must be last 2 bytes)
+dw 0xAA55               
